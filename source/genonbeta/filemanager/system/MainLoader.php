5e152932d0516db532fc3ee4f02bfa835a211b32
Veli Tasalı <Veli Tasal>
2016-06-05 00:30:10 +0300

    PHP5.4 bugs

--- source/genonbeta/filemanager/system/MainLoader.php
+++ source/genonbeta/filemanager/system/MainLoader.php
@@ -10,10 +10,10 @@ use genonbeta\system\System;
 use genonbeta\view\ViewSkeleton;
 use genonbeta\filemanager\config\MainConfig;
 
-class Loader extends \genonbeta\core\system\Loader
+class MainLoader extends \genonbeta\core\system\Loader
 {
 	const TAG = "Loader";
-	
+
 	protected function onCreate()
 	{
 		$fwConf = System::getLoadedManifest()['configuration'];
@@ -23,7 +23,7 @@ class Loader extends \genonbeta\core\system\Loader
 		$req->request(Configuration::DATA_PATH."/".$fwConf['languages_path'], RequiredFiles::TYPE_DIRECTORY);
 		$req->request(Configuration::DATA_PATH."/".$fwConf['htmlPatterns_path'], RequiredFiles::TYPE_DIRECTORY);
 		$req->request(Configuration::DATA_PATH."/".$fwConf['databases_path'], RequiredFiles::TYPE_DIRECTORY);
-		
+
 		ResourceManager::addResource(MainConfig::LANGUAGE_INDEX_NAME, Configuration::DATA_PATH."/".$fwConf['languages_path'], "json");
 		ResourceManager::addResource(MainConfig::PATTERN_INDEX_NAME, Configuration::DATA_PATH."/".$fwConf['htmlPatterns_path'], "html");
 		ResourceManager::addResource(MainConfig::DB_INDEX_NAME, Configuration::DATA_PATH."/".$fwConf['databases_path'], "db");
@@ -32,7 +32,7 @@ class Loader extends \genonbeta\core\system\Loader
 	protected function onSkeletonLoaded(ViewSkeleton $skeleton)
 	{
 		$this->getOutputController()->putIndex("systemOutput", $skeleton);
-		
+
 		// Let's output the data =)
 		echo $this->getOutputController()->onFlush(FlushArgument::getDefaultArguments());
 	}
