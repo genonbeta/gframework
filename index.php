f4debb31524bb714a264ac35b501355401a25db0
Veli <veli.tasali@gmail.com>
2016-10-08 12:10:42 +0300

    Updates

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
 
 /*
-* 'genonbeta framework' version 1.0
-* This is an alfa version of gFramework
-* © 2015 - Genonbeta Open Source Project
-*/
+ * index.php
+ * 
+ * Copyright 2016 Veli TASALI <veli.tasali@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
 
 if (version_compare(PHP_VERSION, '5.3.0', '<'))
 	die("<html>\n<title>Oppps!!!</title>\n	We're sorry but your PHP version is not supported anymore. You're currently running on ".PHP_VERSION." <i>(< 5.3.0)</i>. Please get more information about this error <a href=\"https://github.com/genonbeta/gframework/issues\">https://github.com/genonbeta/gframework/issues</a> \n</html>");
